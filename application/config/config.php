5106029a1b4a2313a650ea36c8b53058e8c27ca4
Fadhiil Rachman <fadhiilrachman@gmail.com>
2018-07-14 12:06:09 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -136,7 +136,7 @@ $config['subclass_prefix'] = 'MY_';
 | Note: This will NOT disable or override the CodeIgniter-specific
 |	autoloading (application/config/autoload.php)
 */
-$config['composer_autoload'] = FALSE;
+$config['composer_autoload'] = '/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -159,13 +159,6 @@ $config['composer_autoload'] = FALSE;
 |
 */
 $config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-';
-
-/*
-| Composer
-|
-*/
-
-$config['composer_autoload'] = "vendor/autoload.php";
 
 /*
 |--------------------------------------------------------------------------
